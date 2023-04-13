1d100bc8330647403b0534389cfdb4be493f5f97
Worrapon <nine6805@gmail.com>
2023-04-13 15:13:29 +0700

    responsive avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import Login from './pages/Login'
 import { useState } from 'react'
 
 const App = () => {
-
     return (
-      <div className="h-screen bg-[#f5e5b8]">
-        <div className="bg-[#b25013] h-fit px-5 py-2 font-bold text-white leading-tight text-3xl">
-            FARK <br />
-            SUE
+        <div className="h-screen bg-[#f5e5b8]">
+            <div className="bg-[#b25013] h-fit px-5 py-2 font-bold text-white leading-tight text-3xl fixed w-screen">
+                FARK <br />
+                SUE
+            </div>
+            <Login />
         </div>
-      </div>
-        
     )
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import avatar from '../assets/peeps-avatar-alpha.png'
 
 const login = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center h-full">
+        <div className="flex flex-col bg-white shadow-md px-5 lg:flex-row mx-10 sm:mx-24 py-8 rounded-3xl w-full max-w-5xl">
+            <div className="max-w-md lg:max-w-lg self-center">
+                <img src={avatar} alt="avatar" className="object-cover"/>
+            </div>
+            <div className="flex-col w-full">
+                <h1 className="text-2xl text-center font-medium sm:text-4xl text-slate-700 px-3 lg:text-start">Login to your Account.</h1>
+                <p className="px-3 text-center lg:text-start">See what is going on with your business.</p>
+            </div>
+        </div>
     </div>
   )
 }
