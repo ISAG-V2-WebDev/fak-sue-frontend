5f7324b315c31b4c1cbc2fbf1647ee1bb2cd8ea0
Worrapon <nine6805@gmail.com>
2023-04-13 12:53:37 +0700

    FARK SUE label

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 const App = () => {
 
     return (
+      <div className="h-screen bg-[#f5e5b8]">
         <div className="bg-[#b25013] h-fit px-5 py-2 font-bold text-white leading-tight text-3xl">
             FARK <br />
             SUE
         </div>
+      </div>
+        
     )
 }
 
