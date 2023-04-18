740d94dc3a1a005ac2eb48665b212a28b9e314fd
Worrapon <nine6805@gmail.com>
2023-04-18 15:48:58 +0700

    list page with scrollbar

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 
 const List = () => {
-  return (
-    <div className="flex justify-center h-screen py-5">
-      <div className="bg-black bg-opacity-25 w-full max-w-2xl h-full rounded-2xl">
-
-      </div>
-    </div>
-  )
+    return (
+        <div className="flex justify-center h-screen py-5">
+            <div className="flex relative overflow-y-auto gap-3 w-full max-w-2xl h-full rounded-2xl ">
+                <div className="flex flex-col gap-3 w-full absolute">
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                    <div className="w-full bg-white rounded-2xl h-28 shadow-lg"></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default List
