10d74a05595bc1eec07558cb5774fcb0fd907af8
Worrapon <nine6805@gmail.com>
2023-04-25 18:08:56 +0700

    add bell and home icon

--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,13 +1,22 @@
-import Card from '../components/Card'
 import Add from '../assets/add.png'
+import bell from '../assets/bellicon.png'
+import Card from '../components/Card'
+import { CardProps } from '../components/Card'
 import FakForm from '../components/FakForm'
 import RubFakForm from '../components/RubFakForm'
 import { useState } from 'react'
-import { CardProps } from '../components/Card'
-const cardDataFromLocalStorage = localStorage.getItem('cardData')
-const cardData = cardDataFromLocalStorage ? JSON.parse(cardDataFromLocalStorage) : [];
+import { Link } from 'react-router-dom'
 
 const CardsPage = () => {
+    const reqCardDataFromLocalStorage = localStorage.getItem('reqCardData')
+    const reqCardData = reqCardDataFromLocalStorage
+        ? JSON.parse(reqCardDataFromLocalStorage)
+        : []
+    const cardDataFromLocalStorage = localStorage.getItem('cardData')
+    const cardData = cardDataFromLocalStorage
+        ? JSON.parse(cardDataFromLocalStorage)
+        : []
+
     const [isRubFakFormVisible, setIsRubFakFormVisible] = useState(false)
     const [isFakFormVisible, setIsFakFormVisible] = useState(false)
 
@@ -19,30 +28,49 @@ const CardsPage = () => {
         setIsRubFakFormVisible(visibility)
     }
 
-    const cards = cardData.map((card : CardProps, index : number) => (
+    const cards = cardData.map((card: CardProps, index: number) => (
         <Card
-          changeFakFormVisibility={changeFakFormVisibility}
-          key={index}
-          restaurantName={card.restaurantName}
-          time={card.time}
-          description={card.description}
-          username={card.username}
+            changeFakFormVisibility={changeFakFormVisibility}
+            key={index}
+            restaurantName={card.restaurantName}
+            time={card.time}
+            description={card.description}
+            username={card.username}
         />
-      ));
-      
+    ))
 
     return (
         <>
+            {reqCardData.length > 0 && (
+                <Link to="/yourreq">
+                    <div className="absolute max-w-[50px] right-20 top-5 w-full cursor-pointer hover:-translate-y-1 transition-transform">
+                        <img src={bell} alt="#" />
+                    </div>
+                </Link>
+            )}
+
             <div className="m-3.5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 {cards}
             </div>
             <div className="fixed max-w-[5rem] bottom-10 right-10 hover:-translate-y-2 transition-transform cursor-pointer z-40">
-                <img src={Add} alt="addBtn" onClick={() => {setIsRubFakFormVisible(true)}}/>
+                <img
+                    src={Add}
+                    alt="addBtn"
+                    onClick={() => {
+                        setIsRubFakFormVisible(true)
+                    }}
+                />
             </div>
 
-            {isRubFakFormVisible && (<RubFakForm changeRubFakFormVisibility={changeRubFakFormVisibility}/>)}
+            {isRubFakFormVisible && (
+                <RubFakForm
+                    changeRubFakFormVisibility={changeRubFakFormVisibility}
+                />
+            )}
 
-            {isFakFormVisible && (<FakForm changeFakFormVisibility={changeFakFormVisibility}/>)}
+            {isFakFormVisible && (
+                <FakForm changeFakFormVisibility={changeFakFormVisibility} />
+            )}
         </>
     )
 }
--- src/pages/YourReq.tsx
+++ src/pages/YourReq.tsx
@@ -1,24 +1,30 @@
-import ReqCard from "../components/ReqCard"
-import { ReqCardProps } from "../components/ReqCard";
-const reqCardDataFromLocalStorage = localStorage.getItem('reqCardData')
-const reqCardData = reqCardDataFromLocalStorage ? JSON.parse(reqCardDataFromLocalStorage) : [];
-
-
+import home from '../assets/white-home-icon-png-21.jpg'
+import ReqCard from '../components/ReqCard'
+import { ReqCardProps } from '../components/ReqCard'
+import { Link } from 'react-router-dom'
 
 const YourReq = () => {
-
-    const reqCards = reqCardData.map((card : ReqCardProps, index : number) => (
+    const reqCardDataFromLocalStorage = localStorage.getItem('reqCardData')
+    const reqCardData = reqCardDataFromLocalStorage
+        ? JSON.parse(reqCardDataFromLocalStorage)
+        : []
+    const reqCards = reqCardData.map((card: ReqCardProps, index: number) => (
         <ReqCard
-          key={index}
-          username={card.username}
-          menuName={card.menuName}
-          quantity={card.quantity}
-          moreInfo={card.moreInfo}
+            key={index}
+            username={card.username}
+            menuName={card.menuName}
+            quantity={card.quantity}
+            moreInfo={card.moreInfo}
         />
-      ));
+    ))
 
     return (
         <div>
+            <Link to="/cardspage">
+                <div className="absolute max-w-[50px] right-20 top-5 w-full cursor-pointer hover:-translate-y-1 transition-transform">
+                    <img src={home} alt="#" />
+                </div>
+            </Link>
             <div className="m-3.5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 {reqCards}
             </div>
